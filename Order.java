6d4eff2c8fe8d30eb70a61cdba71bd938bf82460
s35177-rgb <s35177@pjwstk.edu.pl>
2026-03-29 20:05:44 +0200

    Add Order class

---
+++ Order.java
@@ -0,0 +1,51 @@
+import java.util.ArrayList;
+
+public class Order {
+    private int orderNumber;
+    private CafeCustomer customer;
+    private ArrayList<MenuItem> items;
+    private boolean paid;
+    private static int nextOrderNumber = 1;
+
+    public Order(CafeCustomer customer) {
+        this.orderNumber = nextOrderNumber;
+        nextOrderNumber++;
+        this.customer = customer;
+        this.items = new ArrayList<>();
+        this.paid = false;
+    }
+
+    public void addItem(MenuItem item) {
+        items.add(item);
+    }
+
+    public double calculateTotal() {
+        double total = 0.0;
+        for (MenuItem item : items) {
+            total += item.getPrice();
+        }
+        return total;
+    }
+
+    public int countItems() {
+        return items.size();
+    }
+
+    public void markAsPaid() {
+        paid = true;
+    }
+
+    public static int getNextOrderNumber() {
+        return nextOrderNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "orderNumber=" + orderNumber +
+                ", customer=" + customer +
+                ", items=" + items +
+                ", paid=" + paid +
+                '}';
+    }
+}
